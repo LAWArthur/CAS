94ee63966cc3d2aceee13fdde15cc0b8f697e4cc
LAWArthur <jincai3335_micro@163.com>
2018-06-27 22:00:38 +0800

    Upload files

---
+++ js/game.js
@@ -0,0 +1,39 @@
+﻿var cv, cxt, h, w;
+var age = Math.floor(Math.random() * 50000000000), civDeg, lastCivDeg, cAge = 0, civDegIndex = 0;
+var time, timeSpeed = 100, SEPossiblity = Math.random() * 1000000, SEDev = Math.random() * 1000000;
+var cDegList = ["Hunting Age", "Agriculture Age", "Industrial Age", "Atomic Age", "Infomation Age", "Microcosmic High-demensional Age", "Proton Age", "Universe Philosophy Age"];
+$(function () {
+    cv = $("canvas");
+    cxt = cv[0].getContext("2d");
+    fit();
+    setTimeout(_update, 1);
+})
+$(window).resize(function () {
+    fit();
+});
+function fit() {
+    h = window.innerHeight;
+    w = window.innerWidth;
+    cv.attr({ "height": h, "width": w });
+    cxt.fillStyle = "#000000";
+    cxt.fillRect(0, 0, w, h);
+}
+function _update() {
+    age += timeSpeed;
+    cAge += timeSpeed;
+    civDegIndex += timeSpeed;
+    for (var i = 0; i < timeSpeed; i++) {
+        if (Math.floor(Math.random() * SEPossiblity) == 0) {
+            civDegIndex += Math.floor(Math.random() * SEDev);
+        }
+    }
+    civDeg = cDegList[Math.floor(civDegIndex / 1000000) > 7 ? 7 : Math.floor(civDegIndex / 1000000)];
+    draw();
+    $("#CInfo").text("Universe Age:" + age + " Civilization Age:" + cAge + " Civilization Degree:" + civDeg);
+    setTimeout(_update, time);
+}
+function draw() {
+    var img = new Image();
+    img.src = "images/" + civDeg + ".png";
+    cxt.drawImage(img, 0, 0, w, h);
+}
---
+++ js/main.js
@@ -0,0 +1,51 @@
+﻿// 常量
+
+// 变量
+var cv, cxt, h, w;
+
+// 主代码
+$(function () {
+    // 变量初始化
+    cv = $("canvas");
+    cxt = cv[0].getContext("2d");
+    fit();
+    draw();
+    $(document).click(function (event) {
+        cxt.beginPath();
+        cxt.rect(w / 3, h / 2 - w / 27, w / 3, 2 * w / 27);
+        cxt.closePath();
+        if (cxt.isPointInPath(event.pageX, event.pageY)) {
+            window.location.href = "game.html";
+        }
+    });
+});
+// 设置重绘
+$(window).resize(function () {
+    fit();
+    draw();
+});
+function draw() {
+    cxt.fillStyle = "#ffff99";
+    for (var i = 0; i < 250; i++) {
+        cxt.beginPath();
+        cxt.arc(Math.random() * w, Math.random() * h, Math.random() * 3, 0, Math.PI * 2);
+        cxt.closePath();
+        cxt.fill();
+    }
+    cxt.font = w / 25 + "px Segoe Script";
+    cxt.fillStyle = "#ffffff";
+    cxt.textAlign = "center";
+    cxt.fillText("Civilization & Suspecion", w / 2, h / 5);
+    cxt.font = w / 30 + "px Segoe Script";
+    cxt.fillText("Start", w / 2, h / 2);
+    cxt.beginPath();
+    cxt.rect(w / 3, h / 2 - w / 27, w / 3, 2 * w / 27);
+    cxt.closePath();
+}
+function fit() {
+    h = window.innerHeight;
+    w = window.innerWidth;
+    cv.attr({ "height": h, "width": w });
+    cxt.fillStyle = "#000000";
+    cxt.fillRect(0, 0, w, h);
+}
